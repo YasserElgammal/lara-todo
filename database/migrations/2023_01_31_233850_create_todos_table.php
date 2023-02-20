9625ed538a5a068aa46417795a065dbf89876455
Yasser Elgammal <yasser.elgammal@ymail.com>
2023-02-21 01:24:17 +0200

    besm allah

---
+++ database/migrations/2023_01_31_233850_create_todos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('todos', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description');
+            $table->dateTime('due_date');
+            $table->foreignId('type_id')->constrained('types')->onDelete('cascade');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('todos');
+    }
+};
---
+++ resources/views/front/type/index.blade.php
@@ -0,0 +1,5 @@
+<x-front-layout>
+
+    Here we start
+
+</x-front-layout>
